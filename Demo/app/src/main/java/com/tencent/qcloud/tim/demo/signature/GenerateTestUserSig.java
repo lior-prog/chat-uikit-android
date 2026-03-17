cd223adaf84a9db0bf51d6f2199411ed3b32c972
lior-prog <dingdemicr@outlook.com>
2026-03-17 16:07:03 +0800

    Update GenerateTestUserSig.java

--- Demo/app/src/main/java/com/tencent/qcloud/tim/demo/signature/GenerateTestUserSig.java
+++ Demo/app/src/main/java/com/tencent/qcloud/tim/demo/signature/GenerateTestUserSig.java
@@ -41,7 +41,7 @@ public class GenerateTestUserSig {
      * You can view your SDKAppID after creating an application in the [Tencent Cloud IM console](https://console.intl.cloud.tencent.com/im).
      * SDKAppID uniquely identifies a Tencent Cloud account.
      */
-    public static final int SDKAPPID = 0;
+    public static final int SDKAPPID = 70001118;
 
     /**
      * Signature validity period, which should not be set too short
@@ -61,7 +61,7 @@ public class GenerateTestUserSig {
      * Note: this method is for testing only. Before commercial launch, please migrate the UserSig calculation code and key to your backend server to prevent key disclosure and traffic stealing.
      * Reference: https://intl.cloud.tencent.com/document/product/1047/34385
      */
-    private static final String SECRETKEY = "";
+    private static final String SECRETKEY = "296bcd56d413c66df5503ed79b65a28a293b9c6994d685cb847563d2a3421689";
 
     /**
      * Calculate UserSig
